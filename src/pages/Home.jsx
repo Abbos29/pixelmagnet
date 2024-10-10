a77d8fa9718fa1f564e78a9b1298817063a26cde
Abbos29 <abbosvaliev2003@gmail.com>
2024-10-10 13:49:50 +0500

    feat: new project add header

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+import Header from "../components/layout/Header/Header"
+
+
+const Home = () => {
+  return (
+    <div>
+        <section className="home">
+            <div className="container">
+                <h1>Главная страница</h1>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Home
