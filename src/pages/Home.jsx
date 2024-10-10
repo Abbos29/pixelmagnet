9698bc0ddf51d18d5ced9e68ad71bed73b076719
Abbos29 <abbosvaliev2003@gmail.com>
2024-10-10 15:37:15 +0500

    feat: add logos marquee section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 // import React from 'react'
 
 import Hero from "../components/layout/Hero/Hero"
+import Logos from "../components/layout/Logos/Logos"
 
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Logos />
     </>
   )
 }
