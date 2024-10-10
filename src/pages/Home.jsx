5424dea80efac4141f0b14b3832e6c4abcbb6912
Abbos29 <abbosvaliev2003@gmail.com>
2024-10-10 15:02:30 +0500

    feat: add hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,13 @@
 // import React from 'react'
 
-import Header from "../components/layout/Header/Header"
+import Hero from "../components/layout/Hero/Hero"
 
 
 const Home = () => {
   return (
-    <div>
-        <section className="home">
-            <div className="container">
-                <h1>Главная страница</h1>
-            </div>
-        </section>
-    </div>
+    <>
+      <Hero />
+    </>
   )
 }
 
